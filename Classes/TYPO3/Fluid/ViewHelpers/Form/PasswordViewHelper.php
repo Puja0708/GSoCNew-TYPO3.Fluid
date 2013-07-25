fffa5f0a062093653df60d5686f55637643535ce
Puja0708 <singh.puja92@hotmail.com>
2013-07-26 02:42:07 +0530

    init 2 commit

--- Classes/TYPO3/Fluid/ViewHelpers/Form/PasswordViewHelper.php
+++ Classes/TYPO3/Fluid/ViewHelpers/Form/PasswordViewHelper.php
@@ -12,61 +12,25 @@ namespace TYPO3\Fluid\ViewHelpers\Form;
  *                                                                        */
 
 /**
- * View Helper which creates a simple Password Text Box (<input type="password">).
+ * Renders an <input type="hidden" ...> tag.
  *
  * = Examples =
  *
  * <code title="Example">
- * <f:form.password name="myPassword" />
+ * <f:form.hidden name="myHiddenValue" value="42" />
  * </code>
  * <output>
- * <input type="password" name="myPassword" value="default value" />
+ * <input type="hidden" name="myHiddenValue" value="42" />
  * </output>
+ *
+ * You can also use the "property" attribute if you have bound an object to the form.
+ * See <f:form> for more documentation.
  *
  * @api
  */
-class PasswordViewHelper extends \TYPO3\Fluid\ViewHelpers\Form\AbstractFormFieldViewHelper {
+class HiddenViewHelper extends \TYPO3\Base\ViewHelpers\Form\HiddenViewHelper {
 
-	/**
-	 * @var string
-	 */
-	protected $tagName = 'input';
-
-	/**
-	 * Initialize the arguments.
-	 *
-	 * @return void
-	 * @api
-	 */
-	public function initializeArguments() {
-		parent::initializeArguments();
-		$this->registerTagAttribute('disabled', 'string', 'Specifies that the input element should be disabled when the page loads');
-		$this->registerTagAttribute('maxlength', 'int', 'The maxlength attribute of the input field (will not be validated)');
-		$this->registerTagAttribute('readonly', 'string', 'The readonly attribute of the input field');
-		$this->registerTagAttribute('size', 'int', 'The size of the input field');
-		$this->registerTagAttribute('placeholder', 'string', 'The placeholder of the input field');
-		$this->registerArgument('errorClass', 'string', 'CSS class to set if there are errors for this view helper', FALSE, 'f3-form-error');
-		$this->registerUniversalTagAttributes();
-	}
-
-	/**
-	 * Renders the password input field.
-	 *
-	 * @return string
-	 * @api
-	 */
-	public function render() {
-		$name = $this->getName();
-		$this->registerFieldNameForFormTokenGeneration($name);
-
-		$this->tag->addAttribute('type', 'password');
-		$this->tag->addAttribute('name', $name);
-		$this->tag->addAttribute('value', $this->getValue());
-
-		$this->setErrorClassAttribute();
-
-		return $this->tag->render();
-	}
+	
 }
 
 ?>
