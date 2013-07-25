d436e85a321dec41ab6a12f622aeaf57343989a0
Puja0708 <singh.puja92@hotmail.com>
2013-07-25 11:50:20 +0530

    Update StandaloneView.php

--- Classes/TYPO3/Fluid/View/StandaloneView.php
+++ Classes/TYPO3/Fluid/View/StandaloneView.php
@@ -42,5 +42,39 @@ abstract class StandaloneView extends \TYPO3\Base\View\StandaloneView  implement
 	 * @Flow\Inject
 	 */
 	protected $flashMessageContainer;
+
+	/**
+	 * Add a variable to the view data collection.
+	 * Can be chained, so $this->view->assign(..., ...)->assign(..., ...); is possible
+	 *
+	 * @param string $key Key of variable
+	 * @param mixed $value Value of object
+	 * @return \TYPO3\Flow\Mvc\View\ViewInterface an instance of $this, to enable chaining
+	 * @api
+	 */
+	public function assign($key, $value){
+			;
+	}
+
+	/**
+	 * Add multiple variables to the view data collection
+	 *
+	 * @param array $values array in the format array(key1 => value1, key2 => value2)
+	 * @return \TYPO3\Flow\Mvc\View\ViewInterface an instance of $this, to enable chaining
+	 * @api
+	 */
+	public function assignMultiple(array $values){
+		;
+	}
+
+	/**
+	 * Renders the view
+	 *
+	 * @return string The rendered view
+	 * @api
+	 */
+	public function render() {
+		;
+	}
 }
 ?>
