a0bdebdd6bb684fc18d427a7bf96cfc13b746aa2
Puja0708 <singh.puja92@hotmail.com>
2013-07-27 00:15:06 +0530

    Update HtmlentitiesDecodeViewHelper.php

--- Classes/TYPO3/Fluid/ViewHelpers/Format/HtmlentitiesDecodeViewHelper.php
+++ Classes/TYPO3/Fluid/ViewHelpers/Format/HtmlentitiesDecodeViewHelper.php
@@ -33,36 +33,9 @@ namespace TYPO3\Fluid\ViewHelpers\Format;
  *
  * @api
  */
-class HtmlentitiesDecodeViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
+class HtmlentitiesDecodeViewHelper extends \TYPO3\Base\ViewHelpers\Format\HtmlentitiesDecodeViewHelper {
 
-	/**
-	 * Disable the escaping interceptor because otherwise the child nodes would be escaped before this view helper
-	 * can decode the text's entities.
-	 *
-	 * @var boolean
-	 */
-	protected $escapingInterceptorEnabled = FALSE;
-
-	/**
-	 * Converts all HTML entities to their applicable characters as needed using PHPs html_entity_decode() function.
-	 *
-	 * @param string $value string to format
-	 * @param boolean $keepQuotes if TRUE, single and double quotes won't be replaced (sets ENT_NOQUOTES flag)
-	 * @param string $encoding
-	 * @return string the altered string
-	 * @see http://www.php.net/html_entity_decode
-	 * @api
-	 */
-	public function render($value = NULL, $keepQuotes = FALSE, $encoding = 'UTF-8') {
-		if ($value === NULL) {
-			$value = $this->renderChildren();
-		}
-		if (!is_string($value)) {
-			return $value;
-		}
-		$flags = $keepQuotes ? ENT_NOQUOTES : ENT_COMPAT;
-		return html_entity_decode($value, $flags, $encoding);
-	}
+	
 }
 
 ?>
