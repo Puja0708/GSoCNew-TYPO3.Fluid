7126a3ad2ceaba1dacd0201e2fe50764f0988bc0
Puja0708 <singh.puja92@hotmail.com>
2013-07-27 02:13:07 +0530

    Update RawViewHelper.php

--- Classes/TYPO3/Fluid/ViewHelpers/Format/RawViewHelper.php
+++ Classes/TYPO3/Fluid/ViewHelpers/Format/RawViewHelper.php
@@ -11,59 +11,10 @@ namespace TYPO3\Fluid\ViewHelpers\Format;
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
-/**
- * Outputs an argument/value without any escaping. Is normally used to output
- * an ObjectAccessor which should not be escaped, but output as-is.
- *
- * PAY SPECIAL ATTENTION TO SECURITY HERE (especially Cross Site Scripting),
- * as the output is NOT SANITIZED!
- *
- * = Examples =
- *
- * <code title="Child nodes">
- * <f:format.raw>{string}</f:format.raw>
- * </code>
- * <output>
- * (Content of {string} without any conversion/escaping)
- * </output>
- *
- * <code title="Value attribute">
- * <f:format.raw value="{string}" />
- * </code>
- * <output>
- * (Content of {string} without any conversion/escaping)
- * </output>
- *
- * <code title="Inline notation">
- * {string -> f:format.raw()}
- * </code>
- * <output>
- * (Content of {string} without any conversion/escaping)
- * </output>
- *
- * @api
- */
-class RawViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
 
-	/**
-	 * Disable the escaping interceptor because otherwise the child nodes would be escaped before this view helper
-	 * can decode the text's entities.
-	 *
-	 * @var boolean
-	 */
-	protected $escapingInterceptorEnabled = FALSE;
+class RawViewHelper extends \TYPO3\Base\ViewHelpers\Format\RawViewHelper {
 
-	/**
-	 * @param mixed $value The value to output
-	 * @return string
-	 */
-	public function render($value = NULL) {
-		if ($value === NULL) {
-			return $this->renderChildren();
-		} else {
-			return $value;
-		}
-	}
+	
 }
 
 ?>
