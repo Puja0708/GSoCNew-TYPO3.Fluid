1986c5aa9b066cfee5465895832e11c9ad12ab75
Puja0708 <singh.puja92@hotmail.com>
2013-07-27 01:46:22 +0530

    Update NumberViewHelper.php

--- Classes/TYPO3/Fluid/ViewHelpers/Format/NumberViewHelper.php
+++ Classes/TYPO3/Fluid/ViewHelpers/Format/NumberViewHelper.php
@@ -11,44 +11,10 @@ namespace TYPO3\Fluid\ViewHelpers\Format;
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
-/**
- * Formats a number with custom precision, decimal point and grouped thousands.
- * @see http://www.php.net/manual/en/function.number-format.php
- *
- * = Examples =
- *
- * <code title="Defaults">
- * <f:format.number>423423.234</f:format.number>
- * </code>
- * <output>
- * 423,423.20
- * </output>
- *
- * <code title="With all parameters">
- * <f:format.number decimals="1" decimalSeparator="," thousandsSeparator=".">423423.234</f:format.number>
- * </code>
- * <output>
- * 423.423,2
- * </output>
- *
- * @api
- */
-class NumberViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
 
-	/**
-	 * Format the numeric value as a number with grouped thousands, decimal point and
-	 * precision.
-	 *
-	 * @param int $decimals The number of digits after the decimal point
-	 * @param string $decimalSeparator The decimal point character
-	 * @param string $thousandsSeparator The character for grouping the thousand digits
-	 * @return string The formatted number
-	 * @api
-	 */
-	public function render($decimals = 2, $decimalSeparator = '.', $thousandsSeparator = ',') {
-		$stringToFormat = $this->renderChildren();
-		return number_format($stringToFormat, $decimals, $decimalSeparator, $thousandsSeparator);
-	}
+class NumberViewHelper extends \TYPO3\Base\ViewHelpers\Format\NumberViewHelper {
+
+	
 }
 
 ?>
