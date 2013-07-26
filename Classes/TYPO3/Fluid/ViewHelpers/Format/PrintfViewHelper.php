459d7304821edb4d7439dfc8159d14b080b4ea4b
Puja0708 <singh.puja92@hotmail.com>
2013-07-27 02:10:13 +0530

    Update PrintfViewHelper.php

--- Classes/TYPO3/Fluid/ViewHelpers/Format/PrintfViewHelper.php
+++ Classes/TYPO3/Fluid/ViewHelpers/Format/PrintfViewHelper.php
@@ -11,56 +11,10 @@ namespace TYPO3\Fluid\ViewHelpers\Format;
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
-/**
- * A view helper for formatting values with printf. Either supply an array for
- * the arguments or a single value.
- * See http://www.php.net/manual/en/function.sprintf.php
- *
- * = Examples =
- *
- * <code title="Scientific notation">
- * <f:format.printf arguments="{number: 362525200}">%.3e</f:format.printf>
- * </code>
- * <output>
- * 3.625e+8
- * </output>
- *
- * <code title="Argument swapping">
- * <f:format.printf arguments="{0: 3, 1: 'Kasper'}">%2$s is great, TYPO%1$d too. Yes, TYPO%1$d is great and so is %2$s!</f:format.printf>
- * </code>
- * <output>
- * Kasper is great, TYPO3 too. Yes, TYPO3 is great and so is Kasper!
- * </output>
- *
- * <code title="Single argument">
- * <f:format.printf arguments="{1: 'TYPO3'}">We love %s</f:format.printf>
- * </code>
- * <output>
- * We love TYPO3
- * </output>
- *
- * <code title="Inline notation">
- * {someText -> f:format.printf(arguments: {1: 'TYPO3'})}
- * </code>
- * <output>
- * We love TYPO3
- * </output>
- *
- * @api
- */
-class PrintfViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
 
-	/**
-	 * Format the arguments with the given printf format string.
-	 *
-	 * @param array $arguments The arguments for vsprintf
-	 * @return string The formatted value
-	 * @api
-	 */
-	public function render(array $arguments) {
-		$format = $this->renderChildren();
-		return vsprintf($format, $arguments);
-	}
+class PrintfViewHelper extends \YPO3\Base\ViewHelpers\Format\PrintfViewHelper {
+
+	
 }
 
 ?>
