86e1403aa347c4c238ba52892abe997f8ec6dbf6
Puja0708 <singh.puja92@hotmail.com>
2013-07-26 17:07:35 +0530

    Update TextareaViewHelper.php

--- Classes/TYPO3/Fluid/ViewHelpers/Form/TextareaViewHelper.php
+++ Classes/TYPO3/Fluid/ViewHelpers/Form/TextareaViewHelper.php
@@ -26,47 +26,9 @@ namespace TYPO3\Fluid\ViewHelpers\Form;
  *
  * @api
  */
-class TextareaViewHelper extends \TYPO3\Fluid\ViewHelpers\Form\AbstractFormFieldViewHelper {
+class TextareaViewHelper extends \TYPO3\Base\ViewHelpers\Form\TextareaViewHelper {
 
-	/**
-	 * @var string
-	 */
-	protected $tagName = 'textarea';
 
-	/**
-	 * Initialize the arguments.
-	 *
-	 * @return void
-	 * @api
-	 */
-	public function initializeArguments() {
-		parent::initializeArguments();
-		$this->registerTagAttribute('rows', 'int', 'The number of rows of a text area');
-		$this->registerTagAttribute('cols', 'int', 'The number of columns of a text area');
-		$this->registerTagAttribute('disabled', 'string', 'Specifies that the input element should be disabled when the page loads');
-		$this->registerTagAttribute('placeholder', 'string', 'The placeholder of the textarea');
-		$this->registerArgument('errorClass', 'string', 'CSS class to set if there are errors for this view helper', FALSE, 'f3-form-error');
-		$this->registerUniversalTagAttributes();
-	}
-
-	/**
-	 * Renders the textarea.
-	 *
-	 * @return string
-	 * @api
-	 */
-	public function render() {
-		$name = $this->getName();
-		$this->registerFieldNameForFormTokenGeneration($name);
-
-		$this->tag->forceClosingTag(TRUE);
-		$this->tag->addAttribute('name', $name);
-		$this->tag->setContent(htmlspecialchars($this->getValue()));
-
-		$this->setErrorClassAttribute();
-
-		return $this->tag->render();
-	}
 }
 
 ?>
