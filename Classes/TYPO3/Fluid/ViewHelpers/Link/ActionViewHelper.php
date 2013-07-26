54454844d12deff01f4935fb8ebb160c9291cc5a
Puja0708 <singh.puja92@hotmail.com>
2013-07-27 02:29:01 +0530

    Update ActionViewHelper.php

--- Classes/TYPO3/Fluid/ViewHelpers/Link/ActionViewHelper.php
+++ Classes/TYPO3/Fluid/ViewHelpers/Link/ActionViewHelper.php
@@ -35,74 +35,17 @@ namespace TYPO3\Fluid\ViewHelpers\Link;
  *
  * @api
  */
-class ActionViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper {
+class ActionViewHelper extends \TYPO3\Base\ViewHelpers\Link\ActionViewHelper {
 
-	/**
-	 * @var string
-	 */
-	protected $tagName = 'a';
-
-	/**
-	 * Initialize arguments
-	 *
-	 * @return void
-	 * @api
-	 */
-	public function initializeArguments() {
-		$this->registerUniversalTagAttributes();
-		$this->registerTagAttribute('name', 'string', 'Specifies the name of an anchor');
-		$this->registerTagAttribute('rel', 'string', 'Specifies the relationship between the current document and the linked document');
-		$this->registerTagAttribute('rev', 'string', 'Specifies the relationship between the linked document and the current document');
-		$this->registerTagAttribute('target', 'string', 'Specifies where to open the linked document');
-	}
-
-	/**
-	 * Render the link.
-	 *
-	 * @param string $action Target action
-	 * @param array $arguments Arguments
-	 * @param string $controller Target controller. If NULL current controllerName is used
-	 * @param string $package Target package. if NULL current package is used
-	 * @param string $subpackage Target subpackage. if NULL current subpackage is used
-	 * @param string $section The anchor to be added to the URI
-	 * @param string $format The requested format, e.g. ".html"
-	 * @param array $additionalParams additional query parameters that won't be prefixed like $arguments (overrule $arguments)
-	 * @param boolean $addQueryString If set, the current query parameters will be kept in the URI
-	 * @param array $argumentsToBeExcludedFromQueryString arguments to be removed from the URI. Only active if $addQueryString = TRUE
-	 * @param boolean $useParentRequest If set, the parent Request will be used instead of the current one
-	 * @return string The rendered link
-	 * @throws \TYPO3\Fluid\Core\ViewHelper\Exception
-	 * @api
-	 */
+	
 	public function render($action, $arguments = array(), $controller = NULL, $package = NULL, $subpackage = NULL, $section = '', $format = '',  array $additionalParams = array(), $addQueryString = FALSE, array $argumentsToBeExcludedFromQueryString = array(), $useParentRequest = FALSE) {
-		$uriBuilder = $this->controllerContext->getUriBuilder();
-		if ($useParentRequest) {
-			$request = $this->controllerContext->getRequest();
-			if ($request->isMainRequest()) {
-				throw new \TYPO3\Fluid\Core\ViewHelper\Exception('You can\'t use the parent Request, you are already in the MainRequest.', 1360163536);
-			}
-			$uriBuilder = clone $uriBuilder;
-			$uriBuilder->setRequest($request->getParentRequest());
-		}
-
-		$uriBuilder
-			->reset()
-			->setSection($section)
-			->setCreateAbsoluteUri(TRUE)
-			->setArguments($additionalParams)
-			->setAddQueryString($addQueryString)
-			->setArgumentsToBeExcludedFromQueryString($argumentsToBeExcludedFromQueryString)
-			->setFormat($format);
+		
 		try {
 			$uri = $uriBuilder->uriFor($action, $arguments, $controller, $package, $subpackage);
 		} catch (\TYPO3\Flow\Exception $exception) {
 			throw new \TYPO3\Fluid\Core\ViewHelper\Exception($exception->getMessage(), $exception->getCode(), $exception);
 		}
-		$this->tag->addAttribute('href', $uri);
-		$this->tag->setContent($this->renderChildren());
-		$this->tag->forceClosingTag(TRUE);
-
-		return $this->tag->render();
+		
 	}
 }
 
