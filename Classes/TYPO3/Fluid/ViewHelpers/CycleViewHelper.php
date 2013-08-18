5e1c26d9d7337746e8fb32393d193f26ec76b915
Puja <singh.puja92@hotmail.com>
2013-08-18 20:49:24 +0530

    Update CycleViewHelper.php

--- Classes/TYPO3/Fluid/ViewHelpers/CycleViewHelper.php
+++ Classes/TYPO3/Fluid/ViewHelpers/CycleViewHelper.php
@@ -12,12 +12,42 @@ namespace TYPO3\Fluid\ViewHelpers;
  *                                                                        */
 
 /**
- * "THEN" -> only has an effect inside of "IF". See If-ViewHelper for documentation.
+ * This ViewHelper cycles through the specified values.
+ * This can be often used to specify CSS classes for example.
+ * **Note:** To achieve the "zebra class" effect in a loop you can also use the "iteration" argument of the **for** ViewHelper.
  *
- * @see \TYPO3\Fluid\ViewHelpers\IfViewHelper
+ * = Examples =
+ *
+ * <code title="Simple">
+ * <f:for each="{0:1, 1:2, 2:3, 3:4}" as="foo"><f:cycle values="{0: 'foo', 1: 'bar', 2: 'baz'}" as="cycle">{cycle}</f:cycle></f:for>
+ * </code>
+ * <output>
+ * foobarbazfoo
+ * </output>
+ *
+ * <code title="Alternating CSS class">
+ * <ul>
+ *   <f:for each="{0:1, 1:2, 2:3, 3:4}" as="foo">
+ *     <f:cycle values="{0: 'odd', 1: 'even'}" as="zebraClass">
+ *       <li class="{zebraClass}">{foo}</li>
+ *     </f:cycle>
+ *   </f:for>
+ * </ul>
+ * </code>
+ * <output>
+ * <ul>
+ *   <li class="odd">1</li>
+ *   <li class="even">2</li>
+ *   <li class="odd">3</li>
+ *   <li class="even">4</li>
+ * </ul>
+ * </output>
+ *
+ * Note: The above examples could also be achieved using the "iteration" argument of the ForViewHelper
+ *
  * @api
  */
-class ThenViewHelper extends \TYPO3\Base\ViewHelpers\ThenViewHelper {
+class CycleViewHelper extends \TYPO3\Base\ViewHelpers\CycleViewHelper {
 
 	
 }
