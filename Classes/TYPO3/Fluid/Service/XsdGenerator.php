22eda5b9b2f630e328e00c81e5ff29c475870f9d
Puja <singh.puja92@hotmail.com>
2013-08-24 20:29:56 +0530

    Update XsdGenerator.php

--- Classes/TYPO3/Fluid/Service/XsdGenerator.php
+++ Classes/TYPO3/Fluid/Service/XsdGenerator.php
@@ -17,7 +17,7 @@ use TYPO3\Flow\Annotations as Flow;
  * XML Schema (XSD) Generator. Will generate an XML schema which can be used for autocompletion
  * in schema-aware editors like Eclipse XML editor.
  */
-class XsdGenerator extends \TYPO3\Fluid\Service\AbstractGenerator {
+class XsdGenerator extends \TYPO3\Base\Service\XsdGenerator {
 
 	/**
 	 * @var \TYPO3\Flow\Object\ObjectManagerInterface
@@ -25,34 +25,7 @@ class XsdGenerator extends \TYPO3\Fluid\Service\AbstractGenerator {
 	 */
 	protected $objectManager;
 
-	/**
-	 * Generate the XML Schema definition for a given namespace.
-	 * It will generate an XSD file for all view helpers in this namespace.
-	 *
-	 * @param string $viewHelperNamespace Namespace identifier to generate the XSD for, without leading Backslash.
-	 * @param string $xsdNamespace $xsdNamespace unique target namespace used in the XSD schema (for example "http://yourdomain.org/ns/viewhelpers")
-	 * @return string XML Schema definition
-	 * @throws Exception
-	 */
-	public function generateXsd($viewHelperNamespace, $xsdNamespace) {
-		if (substr($viewHelperNamespace, -1) !== '\\') {
-			$viewHelperNamespace .= '\\';
-		}
-
-		$classNames = $this->getClassNamesInNamespace($viewHelperNamespace);
-		if (count($classNames) === 0) {
-			throw new \TYPO3\Fluid\Service\Exception(sprintf('No ViewHelpers found in namespace "%s"', $viewHelperNamespace), 1330029328);
-		}
-
-		$xmlRootNode = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?>
-			<xsd:schema xmlns:xsd="http://www.w3.org/2001/XMLSchema" targetNamespace="' . $xsdNamespace . '"></xsd:schema>');
-
-		foreach ($classNames as $className) {
-			$this->generateXmlForClassName($className, $viewHelperNamespace, $xmlRootNode);
-		}
-
-		return $xmlRootNode->asXML();
-	}
+	
 
 	/**
 	 * Generate the XML Schema for a given class name.
@@ -85,39 +58,6 @@ class XsdGenerator extends \TYPO3\Fluid\Service\AbstractGenerator {
 		$this->addAttributes($className, $xsdComplexType);
 	}
 
-	/**
-	 * Add attribute descriptions to a given tag.
-	 * Initializes the view helper and its arguments, and then reads out the list of arguments.
-	 *
-	 * @param string $className Class name where to add the attribute descriptions
-	 * @param \SimpleXMLElement $xsdElement XML element to add the attributes to.
-	 * @return void
-	 */
-	protected function addAttributes($className, \SimpleXMLElement $xsdElement) {
-		$viewHelper = $this->objectManager->get($className);
-		$argumentDefinitions = $viewHelper->prepareArguments();
-
-		foreach ($argumentDefinitions as $argumentDefinition) {
-			$xsdAttribute = $xsdElement->addChild('xsd:attribute');
-			$xsdAttribute['type'] = 'xsd:string';
-			$xsdAttribute['name'] = $argumentDefinition->getName();
-			$this->addDocumentation($argumentDefinition->getDescription(), $xsdAttribute);
-			if ($argumentDefinition->isRequired()) {
-				$xsdAttribute['use'] = 'required';
-			}
-		}
-	}
-
-	/**
-	 * Add documentation XSD to a given XML node
-	 *
-	 * @param string $documentation Documentation string to add.
-	 * @param \SimpleXMLElement $xsdParentNode Node to add the documentation to
-	 * @return void
-	 */
-	protected function addDocumentation($documentation, \SimpleXMLElement $xsdParentNode) {
-		$xsdAnnotation = $xsdParentNode->addChild('xsd:annotation');
-		$this->addChildWithCData($xsdAnnotation, 'xsd:documentation', $documentation);
-	}
+	
 }
 ?>
