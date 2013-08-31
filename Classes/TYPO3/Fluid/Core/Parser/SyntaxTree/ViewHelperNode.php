9ae8376e23336770ee428e96a57aad3db2df06da
Puja <singh.puja92@hotmail.com>
2013-08-31 12:53:17 +0530

    Update ViewHelperNode.php

--- Classes/TYPO3/Fluid/Core/Parser/SyntaxTree/ViewHelperNode.php
+++ Classes/TYPO3/Fluid/Core/Parser/SyntaxTree/ViewHelperNode.php
@@ -17,69 +17,9 @@ use TYPO3\Flow\Object\DependencyInjection\DependencyProxy;
 /**
  * Node which will call a ViewHelper associated with this node.
  */
-class ViewHelperNode extends \TYPO3\Fluid\Core\Parser\SyntaxTree\AbstractNode {
+class ViewHelperNode extends \TYPO3\Base\Core\Parser\SyntaxTree\ViewHelperNode {
 
-	/**
-	 * Class name of view helper
-	 *
-	 * @var string
-	 */
-	protected $viewHelperClassName;
-
-	/**
-	 * Arguments of view helper - References to RootNodes.
-	 *
-	 * @var array
-	 */
-	protected $arguments = array();
-
-	/**
-	 * The ViewHelper associated with this node
-	 *
-	 * @var \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper
-	 */
-	protected $uninitializedViewHelper = NULL;
-
-	/**
-	 * A mapping RenderingContext -> ViewHelper to only re-initialize ViewHelpers
-	 * when a context change occurs.
-	 *
-	 * @var \SplObjectStorage
-	 */
-	protected $viewHelpersByContext = NULL;
-
-	/**
-	 * Constructor.
-	 *
-	 * @param \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper $viewHelper The view helper
-	 * @param array $arguments Arguments of view helper - each value is a RootNode.
-	 */
-	public function __construct(\TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper $viewHelper, array $arguments) {
-		$this->uninitializedViewHelper = $viewHelper;
-		$this->viewHelpersByContext = new \SplObjectStorage();
-		$this->arguments = $arguments;
-		$this->viewHelperClassName = ($this->uninitializedViewHelper instanceof DependencyProxy) ? $this->uninitializedViewHelper->_getClassName() : get_class($this->uninitializedViewHelper);
-	}
-
-	/**
-	 * Returns the attached (but still uninitialized) ViewHelper for this ViewHelperNode.
-	 * We need this method because sometimes Interceptors need to ask some information from the ViewHelper.
-	 *
-	 * @return \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper the attached ViewHelper, if it is initialized
-	 */
-	public function getUninitializedViewHelper() {
-		return $this->uninitializedViewHelper;
-	}
-
-	/**
-	 * Get class name of view helper
-	 *
-	 * @return string Class Name of associated view helper
-	 */
-	public function getViewHelperClassName() {
-		return $this->viewHelperClassName;
-	}
-
+	
 	/**
 	 * INTERNAL - only needed for compiling templates
 	 *
@@ -90,61 +30,7 @@ class ViewHelperNode extends \TYPO3\Fluid\Core\Parser\SyntaxTree\AbstractNode {
 		return $this->arguments;
 	}
 
-	/**
-	 * Call the view helper associated with this object.
-	 *
-	 * First, it evaluates the arguments of the view helper.
-	 *
-	 * If the view helper implements \TYPO3\Fluid\Core\ViewHelper\Facets\ChildNodeAccessInterface,
-	 * it calls setChildNodes(array childNodes) on the view helper.
-	 *
-	 * Afterwards, checks that the view helper did not leave a variable lying around.
-	 *
-	 * @param \TYPO3\Fluid\Core\Rendering\RenderingContextInterface $renderingContext
-	 * @return object evaluated node after the view helper has been called.
-	 */
-	public function evaluate(\TYPO3\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
-		if ($this->viewHelpersByContext->contains($renderingContext)) {
-			$viewHelper = $this->viewHelpersByContext[$renderingContext];
-			$viewHelper->resetState();
-		} else {
-			$viewHelper = clone $this->uninitializedViewHelper;
-			$this->viewHelpersByContext->attach($renderingContext, $viewHelper);
-		}
-
-		$evaluatedArguments = array();
-		if (count($viewHelper->prepareArguments())) {
-			foreach ($viewHelper->prepareArguments() as $argumentName => $argumentDefinition) {
-				if (isset($this->arguments[$argumentName])) {
-					$argumentValue = $this->arguments[$argumentName];
-					$evaluatedArguments[$argumentName] = $argumentValue->evaluate($renderingContext);
-				} else {
-					$evaluatedArguments[$argumentName] = $argumentDefinition->getDefaultValue();
-				}
-			}
-		}
-
-		$viewHelper->setArguments($evaluatedArguments);
-		$viewHelper->setViewHelperNode($this);
-		$viewHelper->setRenderingContext($renderingContext);
-
-		if ($viewHelper instanceof \TYPO3\Fluid\Core\ViewHelper\Facets\ChildNodeAccessInterface) {
-			$viewHelper->setChildNodes($this->childNodes);
-		}
-
-		$output = $viewHelper->initializeArgumentsAndRender();
-
-		return $output;
-	}
-
-	/**
-	 * Clean up for serializing.
-	 *
-	 * @return array
-	 */
-	public function __sleep() {
-		return array('viewHelperClassName', 'arguments', 'childNodes');
-	}
+	
 }
 
 ?>
