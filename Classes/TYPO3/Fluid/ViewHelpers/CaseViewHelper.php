32c0a431d526f557e119ddb5ebbd3cc80145fd7c
Puja <singh.puja92@hotmail.com>
2013-09-15 23:20:02 +0530

    Update CaseViewHelper.php

--- Classes/TYPO3/Fluid/ViewHelpers/CaseViewHelper.php
+++ Classes/TYPO3/Fluid/ViewHelpers/CaseViewHelper.php
@@ -2,7 +2,7 @@
 namespace TYPO3\Fluid\ViewHelpers;
 
 /*                                                                        *
- * This script is backported from the FLOW3 package "TYPO3.Fluid".        *
+ * This script belongs to the FLOW3 package "Fluid".                      *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU Lesser General Public License, either version 3   *
@@ -10,23 +10,65 @@ namespace TYPO3\Fluid\ViewHelpers;
  *                                                                        *
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
 /**
- * View helper which creates a <base href="..."></base> tag. The Base URI
- * is taken from the current request.
- * In FLOW3, you should always include this ViewHelper to make the links work.
- *
- * = Examples =
- *
- * <code title="Example">
- * <f:base />
- * </code>
- * <output>
- * <base href="http://yourdomain.tld/" />
- * (depending on your domain)
- * </output>
+ * Case view helper that is only usable within the SwitchViewHelper.
+ * @see \TYPO3\Fluid\ViewHelpers\SwitchViewHelper
  *
  * @api
  */
 class CaseViewHelper extends \TYPO3\Base\ViewHelpers\CaseViewHelper {
+/**
+	 * Controller Context to use
+	 * @var \TYPO3\Flow\Mvc\Controller\ControllerContext
+	 * @api
+	 */
+	protected $controllerContext;
 
+	
+
+	/**
+	 * Reflection service
+	 * @var \TYPO3\Flow\Reflection\ReflectionService
+	 */
+	protected $reflectionService;
+
+	/**
+	 * @var \TYPO3\Flow\Object\ObjectManagerInterface
+	 */
+	protected $objectManager;
+
+	/**
+	 * @var \TYPO3\Flow\Log\SystemLoggerInterface
+	 */
+	protected $systemLogger;
+
+	/**
+	 * @param \TYPO3\Flow\Object\ObjectManagerInterface $objectManager
+	 * @return void
+	 */
+	public function injectObjectManager(\TYPO3\Flow\Object\ObjectManagerInterface $objectManager) {
+		$this->objectManager = $objectManager;
+	}
+
+	/**
+	 * @param \TYPO3\Flow\Log\SystemLoggerInterface $systemLogger
+	 * @return void
+	 */
+	public function injectSystemLogger(\TYPO3\Flow\Log\SystemLoggerInterface $systemLogger) {
+		$this->systemLogger = $systemLogger;
+	}
+
+
+	/**
+	 * Inject a Reflection service
+	 * @param \TYPO3\Flow\Reflection\ReflectionService $reflectionService Reflection service
+	 */
+	public function injectReflectionService(\TYPO3\Flow\Reflection\ReflectionService $reflectionService) {
+		$this->reflectionService = $reflectionService;
+	}
+	
 }
+?>
