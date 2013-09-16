b7b378c73dc15c304b8cba0cba78c2412a31b7db
Puja <singh.puja92@hotmail.com>
2013-09-16 20:48:18 +0530

    Update TranslateViewHelper.php

--- Classes/TYPO3/Fluid/ViewHelpers/TranslateViewHelper.php
+++ Classes/TYPO3/Fluid/ViewHelpers/TranslateViewHelper.php
@@ -63,7 +63,7 @@ use TYPO3\Flow\Annotations as Flow;
  * </output>
  *
  */
-class TranslateViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
+class TranslateViewHelper extends \TYPO3\Base\ViewHelpers\TranslateViewHelper {
 
 	/**
 	 * @var \TYPO3\Flow\I18n\Translator
@@ -114,6 +114,58 @@ class TranslateViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelpe
 			}
 		}
 	}
+
+		/**
+	 * Controller Context to use
+	 * @var \TYPO3\Flow\Mvc\Controller\ControllerContext
+	 * @api
+	 */
+	protected $controllerContext;
+
+	
+
+	/**
+	 * Reflection service
+	 * @var \TYPO3\Flow\Reflection\ReflectionService
+	 */
+	protected $reflectionService;
+
+	/**
+	 * @var \TYPO3\Flow\Object\ObjectManagerInterface
+	 */
+	protected $objectManager;
+
+	/**
+	 * @var \TYPO3\Flow\Log\SystemLoggerInterface
+	 */
+	protected $systemLogger;
+
+	/**
+	 * @param \TYPO3\Flow\Object\ObjectManagerInterface $objectManager
+	 * @return void
+	 */
+	public function injectObjectManager(\TYPO3\Flow\Object\ObjectManagerInterface $objectManager) {
+		$this->objectManager = $objectManager;
+	}
+
+	/**
+	 * @param \TYPO3\Flow\Log\SystemLoggerInterface $systemLogger
+	 * @return void
+	 */
+	public function injectSystemLogger(\TYPO3\Flow\Log\SystemLoggerInterface $systemLogger) {
+		$this->systemLogger = $systemLogger;
+	}
+
+
+	/**
+	 * Inject a Reflection service
+	 * @param \TYPO3\Flow\Reflection\ReflectionService $reflectionService Reflection service
+	 */
+	public function injectReflectionService(\TYPO3\Flow\Reflection\ReflectionService $reflectionService) {
+		$this->reflectionService = $reflectionService;
+	}
+
+
 }
 
 ?>
