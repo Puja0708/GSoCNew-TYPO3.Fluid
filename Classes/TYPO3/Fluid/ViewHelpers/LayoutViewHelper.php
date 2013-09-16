4a2a1ad3afc4370f2a3fc68ca8c23af0c82c9400
Puja <singh.puja92@hotmail.com>
2013-09-16 20:35:17 +0530

    Update LayoutViewHelper.php

--- Classes/TYPO3/Fluid/ViewHelpers/LayoutViewHelper.php
+++ Classes/TYPO3/Fluid/ViewHelpers/LayoutViewHelper.php
@@ -11,6 +11,9 @@ namespace TYPO3\Fluid\ViewHelpers;
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
+use TYPO3\Flow\Annotations as Flow;
+
+
 /**
  * With this tag, you can select a layout to be used for the current template.
  *
@@ -26,6 +29,58 @@ namespace TYPO3\Fluid\ViewHelpers;
  * @api
  */
 class LayoutViewHelper extends \TYPO3\Base\ViewHelpers\LayoutViewHelper {
+
+
+	
+	/**
+	 * Controller Context to use
+	 * @var \TYPO3\Flow\Mvc\Controller\ControllerContext
+	 * @api
+	 */
+	protected $controllerContext;
+
+	
+
+	/**
+	 * Reflection service
+	 * @var \TYPO3\Flow\Reflection\ReflectionService
+	 */
+	protected $reflectionService;
+
+	/**
+	 * @var \TYPO3\Flow\Object\ObjectManagerInterface
+	 */
+	protected $objectManager;
+
+	/**
+	 * @var \TYPO3\Flow\Log\SystemLoggerInterface
+	 */
+	protected $systemLogger;
+
+	/**
+	 * @param \TYPO3\Flow\Object\ObjectManagerInterface $objectManager
+	 * @return void
+	 */
+	public function injectObjectManager(\TYPO3\Flow\Object\ObjectManagerInterface $objectManager) {
+		$this->objectManager = $objectManager;
+	}
+
+	/**
+	 * @param \TYPO3\Flow\Log\SystemLoggerInterface $systemLogger
+	 * @return void
+	 */
+	public function injectSystemLogger(\TYPO3\Flow\Log\SystemLoggerInterface $systemLogger) {
+		$this->systemLogger = $systemLogger;
+	}
+
+
+	/**
+	 * Inject a Reflection service
+	 * @param \TYPO3\Flow\Reflection\ReflectionService $reflectionService Reflection service
+	 */
+	public function injectReflectionService(\TYPO3\Flow\Reflection\ReflectionService $reflectionService) {
+		$this->reflectionService = $reflectionService;
+	}
 
 	
 }
