9f98642227a7fde3fe9888c12b09c750cef86ffa
Puja <singh.puja92@hotmail.com>
2013-09-16 20:40:08 +0530

    Update SectionViewHelper.php

--- Classes/TYPO3/Fluid/ViewHelpers/SectionViewHelper.php
+++ Classes/TYPO3/Fluid/ViewHelpers/SectionViewHelper.php
@@ -11,6 +11,8 @@ namespace TYPO3\Fluid\ViewHelpers;
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
+use TYPO3\Flow\Annotations as Flow;
+
 /**
  * A Section view helper
  *
@@ -53,9 +55,60 @@ namespace TYPO3\Fluid\ViewHelpers;
  *
  * @api
  */
-class SectionViewHelper extends \TYPO3\Base\ViewHelpers\SectionViewHelper {
+class SectionViewHelper extends \TYPO3\Base\ViewHelpers\SectionViewHelper  {
 
 	
+	/**
+	 * Controller Context to use
+	 * @var \TYPO3\Flow\Mvc\Controller\ControllerContext
+	 * @api
+	 */
+	protected $controllerContext;
+
+	
+
+	/**
+	 * Reflection service
+	 * @var \TYPO3\Flow\Reflection\ReflectionService
+	 */
+	protected $reflectionService;
+
+	/**
+	 * @var \TYPO3\Flow\Object\ObjectManagerInterface
+	 */
+	protected $objectManager;
+
+	/**
+	 * @var \TYPO3\Flow\Log\SystemLoggerInterface
+	 */
+	protected $systemLogger;
+
+	/**
+	 * @param \TYPO3\Flow\Object\ObjectManagerInterface $objectManager
+	 * @return void
+	 */
+	public function injectObjectManager(\TYPO3\Flow\Object\ObjectManagerInterface $objectManager) {
+		$this->objectManager = $objectManager;
+	}
+
+	/**
+	 * @param \TYPO3\Flow\Log\SystemLoggerInterface $systemLogger
+	 * @return void
+	 */
+	public function injectSystemLogger(\TYPO3\Flow\Log\SystemLoggerInterface $systemLogger) {
+		$this->systemLogger = $systemLogger;
+	}
+
+
+	/**
+	 * Inject a Reflection service
+	 * @param \TYPO3\Flow\Reflection\ReflectionService $reflectionService Reflection service
+	 */
+	public function injectReflectionService(\TYPO3\Flow\Reflection\ReflectionService $reflectionService) {
+		$this->reflectionService = $reflectionService;
+	}
+
+
 }
 
 ?>
