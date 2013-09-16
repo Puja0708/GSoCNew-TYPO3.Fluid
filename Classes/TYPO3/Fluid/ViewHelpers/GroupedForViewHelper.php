69cc6f92bef5e442bb06c09a0fa6dd7a0483a963
Puja <singh.puja92@hotmail.com>
2013-09-16 20:04:49 +0530

    Update GroupedForViewHelper.php

--- Classes/TYPO3/Fluid/ViewHelpers/GroupedForViewHelper.php
+++ Classes/TYPO3/Fluid/ViewHelpers/GroupedForViewHelper.php
@@ -11,6 +11,9 @@ namespace TYPO3\Fluid\ViewHelpers;
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
+
+use TYPO3\Flow\Annotations as Flow;
+
 /**
  * Grouped loop view helper.
  * Loops through the specified values.
@@ -74,6 +77,56 @@ namespace TYPO3\Fluid\ViewHelpers;
  */
 class GroupedForViewHelper extends \TYPO3\Base\ViewHelpers\GroupedForViewHelper {
 
+
+/**
+	 * Controller Context to use
+	 * @var \TYPO3\Flow\Mvc\Controller\ControllerContext
+	 * @api
+	 */
+	protected $controllerContext;
+
+	
+
+	/**
+	 * Reflection service
+	 * @var \TYPO3\Flow\Reflection\ReflectionService
+	 */
+	protected $reflectionService;
+
+	/**
+	 * @var \TYPO3\Flow\Object\ObjectManagerInterface
+	 */
+	protected $objectManager;
+
+	/**
+	 * @var \TYPO3\Flow\Log\SystemLoggerInterface
+	 */
+	protected $systemLogger;
+
+	/**
+	 * @param \TYPO3\Flow\Object\ObjectManagerInterface $objectManager
+	 * @return void
+	 */
+	public function injectObjectManager(\TYPO3\Flow\Object\ObjectManagerInterface $objectManager) {
+		$this->objectManager = $objectManager;
+	}
+
+	/**
+	 * @param \TYPO3\Flow\Log\SystemLoggerInterface $systemLogger
+	 * @return void
+	 */
+	public function injectSystemLogger(\TYPO3\Flow\Log\SystemLoggerInterface $systemLogger) {
+		$this->systemLogger = $systemLogger;
+	}
+
+
+	/**
+	 * Inject a Reflection service
+	 * @param \TYPO3\Flow\Reflection\ReflectionService $reflectionService Reflection service
+	 */
+	public function injectReflectionService(\TYPO3\Flow\Reflection\ReflectionService $reflectionService) {
+		$this->reflectionService = $reflectionService;
+	}
 	
 }
 
